1d47226eb23fd995716a05f672f2fd0bf8c1a798
Menmamen <carmenmontalvoluque@gmail.com>
2025-10-16 21:04:22 +0200

    new prompts

---
+++ ejercicios/CML_estructurasRepetitivas.py
@@ -0,0 +1,22 @@
+# Python exercises for AI Programming Course: Ejercicios de Estructuras Repetitivas
+# Exercises solved by Carmen Montalvo Luque
+
+# 1. Escribir un programa que imprima todos los números pares entre dos números que se le pidan al usuario.
+
+# 2. Realizar un algoritmo que pida números (se pedirá por teclado la cantidad de números a introducir). El programa debe informar de cuantos números introducidos son mayores que 0, menores que 0 e iguales a 0.
+
+# 3. Crea una aplicación que permita adivinar un número. La aplicación genera un número aleatorio del 1 al 100. A continuación va pidiendo números y va respondiendo si el número a adivinar es mayor o menor que el introducido, además de los intentos que te quedan (tienes 10 intentos para acertarlo). El programa termina cuando se acierta el número (además te dice en cuantos intentos lo has acertado), si se llega al limite de intentos te muestra el número que había generado.
+
+# 4. Escribe un programa que pida el limite inferior y superior de un intervalo. Si el límite inferior es mayor que el superior lo tiene que volver a pedir. A continuación se van introduciendo números hasta que introduzcamos el 0. Cuando termine el programa dará las siguientes informaciones:
+# La suma de los números que están dentro del intervalo (intervalo abierto).
+# Cuantos números están fuera del intervalo.
+# Informa si hemos introducido algún número igual a los límites del intervalo.
+
+# 5. Crea un programa que pida un número por teclado al usuario y diga si es primo. En caso de que no se introduzca un número o que esta sea menor que 2 se debe mostrar un mensaje de error.
+
+# 6. Crea un programa que muestre en pantalla los N primeros número primos. El valor de N se pide por teclado al usuario/a.
+
+# 7. Crea un programa que nos permita calcular la cuota de una hipoteca y su tabla de amortización después de que se pida al usuario/a:
+# Importe del préstamo.
+# La tasa de interés anual.
+# Y el plazo de pago en años.
